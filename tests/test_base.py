5d06bc8c65ab2e47dbd57843f91ac919eacdde37
Simon_Staehli <simon.staehli@students.fhnw.ch>
2022-04-11 11:09:09 +0200

    Add base test file

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,15 +2,21 @@ import unittest
 import sys
 sys.path.append('..')
 from UnsplashAPI.base import UnsplashBase
+import os
+
+access_key = os.environ['API_ACCESSKEY']
 
 class TestUnsplashBase(unittest.TestCase):
-    # How to load access key
-    access_key = ''
 
     def test_init(self):
-        base = UnsplashBase(access_key=self.access_key)
+        base = UnsplashBase(access_key=access_key)
         self.assertIsInstance(base, UnsplashBase)
 
     def test_check_api(self):
-        base = UnsplashBase(access_key=self.access_key)
-        base.check_status()
+        base = UnsplashBase(access_key=access_key + 'wrong key')
+        self.assertRaises(AssertionError, base.check_status())
+
+    def test_check_api_con(self):
+        base = UnsplashBase(access_key=access_key)
+        api_status = base.check_status()
+        self.assertEqual(200, api_status)
