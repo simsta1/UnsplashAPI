f745cf5626d95297041bb78b0a35bea987389d46
Simon_Staehli <simon.staehli@students.fhnw.ch>
2022-04-16 18:24:08 +0200

    Enhance docstring of interface class

--- UnsplashAPI/api.py
+++ UnsplashAPI/api.py
@@ -12,8 +12,23 @@ class UnsplashAPI(UnsplashCollections, UnsplashPhotos,
 
     def __init__(self, access_key):
         """
+        Inofficial Wrapper class for the Unsplash API. 
+        
+        All functions are named like the official endpoints, which can be found here: https://unsplash.com/documentation
+
+        To get an access key:
+            - Create Developer Account on Unsplash.com
+            - Add new Application
+            - Adjust "permissions" for your application
+            - copy the "access key" in "Keys"
+
         Args:
-            access_key (_type_): _description_
+            access_key (str):   Access key for application from offifical API.
+
+        Usage:
+            api = UnsplashAPI(access_key='<your key>')
+            api.get_current_rate_limit()
+            >>> '49'
         """
         UnsplashCollections.__init__(self, access_key=access_key)
         UnsplashPhotos.__init__(self, access_key=access_key)
