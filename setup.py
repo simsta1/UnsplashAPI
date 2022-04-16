2cb60352aeea023634259760a27ec06e2d48b7c0
Simon_Staehli <simon.staehli@students.fhnw.ch>
2022-04-16 16:38:57 +0200

    change folder of setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Inofficial API Wrapper for Unsplash API'
+LONG_DESCRIPTION = 'A package which allow python users to easily interact with the UnplashAPI'
+
+# Setting up
+setup(
+    name="unplashapi",
+    version=VERSION,
+    author="SimonStaehli",
+    author_email="<simon.staehli@students.fhnw.ch>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'api', 'unsplash api', 'unsplash'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
